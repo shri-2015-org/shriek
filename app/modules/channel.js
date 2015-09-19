019c3f671640b1a7ae489a1203b2864fd400438f
vitaliy-pavlenko <theatre-of-illusion@yandex.ru>
2015-09-19 19:00:31 +0300

    wrap channel methods around promises

--- app/modules/channel.js
+++ app/modules/channel.js
@@ -11,29 +11,38 @@ var channelModule = function(socket) {
   */
   socket.on('channel create', function (data) {
 
-  slug = slugify(data.name, {lowercase: true, separator: '_'}); // трансилитирируем name
+    var createChannel = new Promise(function(resolve, reject) {
+      slug = slugify(data.name, {lowercase: true, separator: '_'}); // трансилитирируем name
 
-    var newChannel = ChannelModel({
-      name: data.name,
-      slug: slug
+      var newChannel = ChannelModel({
+        name: data.name,
+        slug: slug
+      });
+
+      newChannel.save({runValidators: true}, function (err, data) {
+        console.log(err);
+
+        var out = {};
+        if (!err) {
+          out.status = 'ok';
+          out.channel = data; // здесь будет запись из БД со всеми полями (см схему)
+          resolve(out);
+        } else {
+          var error = new Error('Ошибка создания чата');
+          reject(error);
+        }
+
+        socket.emit('channel create', out);
+      });
     });
 
-    newChannel.save({runValidators: true}, function (err, data) {
-      console.log(err);
-
-      var out = {};
-      if (!err) {
-        out.status = 'ok';
-        out.channel = data; // здесь будет запись из БД со всеми полями (см схему)
-      } else {
-        out.status = 'error';
-        out.error_message = 'Ошибка создания чата';
-      }
-
-      if (out.status == 'ok') socket.broadcast.emit('channel create', out); // броадкастим на всех, только если все прошло удачно
-      socket.emit('channel create', out);
-
-    });
+    createChannel
+    .then(function(data){
+        socket.broadcast.emit('channel create', out); // броадкастим на всех, только если все прошло удачно
+      })
+    .catch(function(error){
+        console.log(error);
+      })
 
   });
 
@@ -43,41 +52,54 @@ var channelModule = function(socket) {
   */
   socket.on('channel info', function (data) {
 
-    ChannelModel.findOne({ slug: data.slug }, function (err, data) {
-
-      var out = {};
-      if (!err) {
-        out.status = 'ok';
-        out.channel = data;
-      } else {
-        out.status = 'error';
-        out.error_message = 'Ошибка получение чата';
-      }
-
-      socket.emit('channel info', out);
-
+    var getChannelInfo = new Promise(function(resolve, reject) {
+      ChannelModel.findOne({ slug: data.slug }, function (err, data) {
+        var out = {};
+        if (!err) {
+          out.status = 'ok';
+          out.channel = data;
+          resolve(out);
+        } else {
+          var error = new Error('Ошибка получения чата');
+          reject(error);
+        }
+      });
     });
+
+    getChannelInfo
+    .then(function(data){
+        socket.emit('channel info', out);
+      })
+    .catch(function(error){
+        console.log(error);
+      });
 
   });
 
   /** Получение всех чатов
   */
   socket.on('channel list', function () {
-
-    ChannelModel.find(function (err, data) {
-
-      var out = {};
-      if (!err) {
-        out.status = 'ok';
-        out.channels = data;
-      } else {
-        out.status = 'error';
-        out.error_message = 'Ошибка получения чатов';
-      }
-
-      socket.emit('channel list', out);
-
+    var getChannelList = new Promise(function (resolve, reject) {
+      ChannelModel.find(function (err, data) {
+        var out = {};
+        if (!err) {
+          out.status = 'ok';
+          out.channels = data;
+          resolve(out);
+        } else {
+          var error = new Error('Ошибка получения чатов');
+          reject(error);
+        }
+      });
     });
+
+    getChannelList
+    .then(function(data){
+        socket.emit('channel list', data);
+      })
+    .catch(function(error){
+        console.log(error);
+      });
 
   });
 
@@ -93,30 +115,36 @@ var channelModule = function(socket) {
     // строим запрос в БД
     var indata = data;
 
-    var query = {
-      channel: data.channel // канал нужно учитывать всегда
-    };
-    if (data.date !== undefined) query.created_at = { $lt: data.date }; // дата — если пришла
+    var getMessages = new Promise(function(resolve, reject){
+      var query = {
+        channel: data.channel // канал нужно учитывать всегда
+      };
+      if (data.date !== undefined) query.created_at = { $lt: data.date }; // дата — если пришла
+      var q = MessageModel.find(query);
+      if (data.limit !== undefined) q.limit(data.limit); // limit
+      if (data.skip !== undefined) q.skip(data.skip); // offset
 
-    var q = MessageModel.find(query);
-
-    if (data.limit !== undefined) q.limit(data.limit); // limit
-    if (data.skip !== undefined) q.skip(data.skip); // offset
-
-    q.exec(function (err, data) { // выполняем запрос
-
-      var out = {};
-      if (!err) {
-        out.status = 'ok';
-        out.messages = (data.length > 0?data:[]); // возвращаем пустой массив или сообщения (чтобы не возвращать null)
-        out.slug = indata.channel;
-      } else {
-        out.status = 'error';
-        out.error_message = 'Ошибка получения сообщений';
-      }
-      socket.emit('channel get', out);
-
+      q.exec(function (err, data) { // выполняем запрос
+        var out = {};
+        if (!err) {
+          out.status = 'ok';
+          out.messages = (data.length > 0?data:[]); // возвращаем пустой массив или сообщения (чтобы не возвращать null)
+          out.slug = indata.channel;
+          resolve(out);
+        } else {
+          var error = new Error('Ошибка получения сообщений');
+          reject(error);
+        }
+      });
     });
+
+    getMessages
+      .then( function(data) {
+        return socket.emit('channel get', data);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
 
   });
 

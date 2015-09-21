73171dba26f4660772addf23a7fed76a3479fd97
Igor Stepanov <sigorilla@gmail.com>
2015-09-22 01:24:01 +0300

    add field for settings

--- app/views/components/setting.jsx
+++ app/views/components/setting.jsx
@@ -6,6 +6,10 @@ var SettingComponent = function (socket) {
       return {
         email: '',
         image: '',
+        first_name: '',
+        last_name: '',
+        sex: '',
+        description: '',
         opened: false
       };
     },
@@ -22,7 +26,11 @@ var SettingComponent = function (socket) {
         if (data.status === 'ok') {
           _this.setState({
             email: data.user.setting.email,
-            image: data.user.setting.image
+            image: data.user.setting.image,
+            first_name: data.user.setting.first_name,
+            last_name: data.user.setting.last_name,
+            description: data.user.setting.description,
+            sex: data.user.setting.sex
           });
         }
       });
@@ -43,6 +51,22 @@ var SettingComponent = function (socket) {
       this.setState({image: e.target.value});
     },
 
+    handleFirstNameChange: function (e) {
+      this.setState({first_name: e.target.value});
+    },
+
+    handleLastNameChange: function (e) {
+      this.setState({last_name: e.target.value});
+    },
+
+    handleSexChange: function (e) {
+      this.setState({sex: e.target.value});
+    },
+
+    handleDescriptionChange: function (e) {
+      this.setState({description: e.target.value});
+    },
+
     handleSave: function (e) {
       e.preventDefault();
 
@@ -52,7 +76,11 @@ var SettingComponent = function (socket) {
           username: socket.username,
           setting: {
             email: this.state.email,
-            image: this.state.image
+            image: this.state.image,
+            first_name: this.state.first_name,
+            last_name: this.state.last_name,
+            description: this.state.description,
+            sex: this.state.sex
           }
         });
       }
@@ -66,7 +94,15 @@ var SettingComponent = function (socket) {
       var formSetting;
 
       formSetting = (
-        <form className="form modal__body" onSubmit={this.handleSave}>
+        <form className="form modal__body setting" onSubmit={this.handleSave}>
+          <div className="form__row">
+            <label className="form__label" htmlFor="inputFirstName"><i className="fa fa-edit"></i></label>
+            <input className="form__text" onChange={this.handleFirstNameChange} type="text" id="inputFirstName" placeholder="First name" value={this.state.first_name} />
+          </div>
+          <div className="form__row">
+            <label className="form__label" htmlFor="inputLastName"><i className="fa fa-edit"></i></label>
+            <input className="form__text" onChange={this.handleLastNameChange} type="text" id="inputLastName" placeholder="Last name" value={this.state.last_name} />
+          </div>
           <div className="form__row">
             <label className="form__label" htmlFor="inputEmail"><i className="fa fa-envelope-o"></i></label>
             <input className="form__text" onChange={this.handleEmailChange} type="email" id="inputEmail" placeholder="Email" value={this.state.email} />
@@ -74,6 +110,21 @@ var SettingComponent = function (socket) {
           <div className="form__row">
             <label className="form__label" htmlFor="inputImage"><i className="fa fa-picture-o"></i></label>
             <input className="form__text" onChange={this.handleImageChange} type="url" id="inputImage" placeholder="Url of Image" value={this.state.image} />
+          </div>
+          <div className="form__row form__row-radio">
+            <span className="form__label"><i className="fa fa-venus-mars"></i></span>
+            <input className="form__radio" name="sex" onChange={this.handleSexChange} type="radio" id="inputSexMale" value="male" defaultChecked={this.state.sex === 'male'} />
+            <label htmlFor="inputSexMale" className="btn">
+              <i className="fa fa-mars"></i>
+            </label>
+            <input className="form__radio" name="sex" onChange={this.handleSexChange} type="radio" id="inputSexFemale" value="female" defaultChecked={this.state.sex === 'female'} />
+            <label htmlFor="inputSexFemale" className="btn">
+              <i className="fa fa-venus"></i>
+            </label>
+          </div>
+          <div className="form__row">
+            <label className="form__label" htmlFor="inputDescription"><i className="fa fa-edit"></i></label>
+            <textarea className="form__textarea" onChange={this.handleDescriptionChange} id="inputDescription" placeholder="Description" value={this.state.description} />
           </div>
           <button className="btn" onClick={this.handleSave} type="submit">Update</button>
           <span> </span>

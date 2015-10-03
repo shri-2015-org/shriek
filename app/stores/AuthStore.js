21dc9a96c9d2f5489bf5ed6a9ad5e1778417a710
TehZarathustra <tehzarathustra@gmail.com>
2015-10-03 18:36:18 +0300

    alt-js-login-codestyle

--- app/stores/AuthStore.js
+++ app/stores/AuthStore.js
@@ -4,24 +4,21 @@ var alt_obj = require('./../controllers/alt_obj');
 var AuthActions = require('./../actions/AuthActions');
 
 function AuthStore() {
-  this.messages = []; // это бывший initState у компонента
-  this.displayName = 'AuthStore'; // обязательное поле для ES5
+  this.messages = [];
+  this.displayName = 'AuthStore';
   this.bindListeners({
-  	logOut: AuthActions.LOG_OUT
+    logOut: AuthActions.LOG_OUT
   });
 }
 
-
-// тут описываем все функции стора (в основном это присваение стейта нового значения)
 AuthStore.prototype.logOut = function (newState) {
-	var _this = this;
-	for (key in newState) {
-		_this[key] = newState[key];
-	}
-	localStorage.removeItem('userName');
-    localStorage.removeItem('userPass');
+  var _this = this;
+  for (key in newState) {
+    _this[key] = newState[key];
+  }
+  localStorage.removeItem('userName');
+  localStorage.removeItem('userPass');
 };
-
 
   return alt_obj.createStore(AuthStore);
 };
